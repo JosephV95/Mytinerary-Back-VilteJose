3ed2ab541fa41e118412c2c70c05ebf29c54ed7e
JosephV95 <viltejl@gmail.com>
2023-08-15 13:00:03 -0300

    Se terminaron los endpoint para el CRUD

--- src/controllers/cities.Controller.js
+++ src/controllers/cities.Controller.js
@@ -1,28 +1,29 @@
 const Cities = require("../models/Cities.js")
-const readCities = (req, res) => {
-  res.json({
-    cities: [
-      {
-        nation: "Japan",
-        city: "Akiha",
-      },
-      {
-        nation: "Japan",
-        city: "Tokio",
-      },
-      {
-        nation: "Japan",
-        city: "Shibuya",
-      },
-    ],
-  });
+
+// Ya que se conectaran con la base de datos, se deben crear funciones Asincronas
+const readCities = async (req, res) => {
+  try {
+    let allCities = await Cities.find()
+    return res.status(200).json({
+      mensaje: "todas las ciudades",
+      allCities
+    })
+  } catch (error) {
+    
+  }
 };
 
-const readCity = (req, res) => {
-  res.json({
-    nation: "Argentina",
-    city: "Mendoza",
-  });
+const readCity = async (req, res) => {
+ try {
+  
+  let oneCity = await Cities.findById(req.params.id)
+  return res.status(200).json({
+    oneCity
+  })
+  
+ } catch (error) {
+  return req.status(500)
+ }
 };
 
 const createCity = async (req, res) => {
@@ -33,10 +34,7 @@ const createCity = async (req, res) => {
       mensaje: " se creo correctamente XD",
       payload
     })
-    // res.status(201).json({
-    //   mensaje: " se creo correctamente XD",
-    //   payload
-    // })
+  
   } catch (error) {()=>{
     res.status(500).json({
       Response: "error"
@@ -44,4 +42,29 @@ const createCity = async (req, res) => {
   }}
 };
 
-module.exports = { readCities, readCity, createCity };
+const updateCity = async (req, res) =>{
+  try {
+    let upCity = await Cities.updateOne(req.body)
+    await Cities.findByIdAndUpdate(req.params.id, upCity)
+    return res.status(200).json({
+      mensaje : "se actualizo",
+      
+  })
+  } catch (error) {
+    
+  }
+};
+// De forma alternativa se pasara el id por query para el Delete
+const deleteCity = async (req, res) =>{
+  try {
+    let {id} = req.query;
+    await Cities.findByIdAndDelete({_id:id})
+    return res.status(200).json({
+      mensaje : "se borro",
+    })
+  } catch (error) {
+    
+  }
+};
+
+module.exports = { readCities, readCity, createCity, updateCity, deleteCity};
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const {readCities, readCity, createCity} = require('../controllers/cities.Controller')
+const {readCities, readCity, createCity, updateCity, deleteCity} = require('../controllers/cities.Controller')
 
 const router = express.Router();
 
 router.get("/cities", readCities);
-router.get("/city", readCity)
-router.post("/cities", createCity)
+router.get("/cities/:id", readCity);
+router.post("/cities", createCity);
+router.put("/cities/:id", updateCity);
+router.delete("/cities", deleteCity);
 
 
 module.exports = router
