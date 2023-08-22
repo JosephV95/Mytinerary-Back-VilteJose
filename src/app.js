c361a667719496c32142783c3a964bc4c41e3c0b
JosephV95 <viltejl@gmail.com>
2023-08-22 15:22:13 -0300

    Se arreglo problema de cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const dotenv = require('dotenv/config.js')
 const express = require("express");
 const router = require("./routes/router")
+const cors = require("cors");
 // Conectara con la database y enviara lo console.log
 require("./config/database");
-const cors = require('cors');
+
 
 const server = express()
 
@@ -12,14 +13,14 @@ const errorHandler = require("./middlewares/errorHandler")
 
 // Necesario para que pueda recibir datos(en json) por medio del body en las solicitudes http
 server.use(express.json())
+// El cors debe ir antes de las rutas para que funcione correctamente
+server.use(cors())
 
 server.use("/api", router)
 
 // Middlewares que manejaran los errores
 server.use(notFound)
 server.use(errorHandler)
-
-server.use(cors());
 
 server.listen(process.env.PORT , ()=>{
     console.log(`Servidor listo en puerto : ${process.env.PORT}`);
