6ca2bfc33139ce48ebca974c7fc05cd6d7a28507
JosephV95 <viltejl@gmail.com>
2023-08-30 13:22:45 -0300

    Se crearon las rutas para itinerarios

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const {readCities, readCity, createCity, updateCity, deleteCity, createMany} = require('../controllers/cities.Controller');
 const {verifyDataCity, verifyDataMany} = require("../middlewares/verifyDataCity");
-const { createItinerary, readItineraries } = require("../controllers/itineraries.Controller");
+const { createItinerary, readItineraries, readItinerary, deleteItinenary, updateItinerary } = require("../controllers/itineraries.Controller");
 
 const router = express.Router();
 
@@ -14,5 +14,8 @@ router.delete("/cities", deleteCity);
 
 router.post("/itineraries", createItinerary);
 router.get("/itineraries", readItineraries);
+router.get("/itineraries/:id", readItinerary);
+router.put("/itineraries/:id", updateItinerary);
+router.delete("/itineraries", deleteItinenary);
 
 module.exports = router
