8d43ff01d58f63d22efc89e8c8a29ff41762ded5
JosephV95 <viltejl@gmail.com>
2023-09-01 15:45:13 -0300

    Se agrego informacion al modelo

--- src/models/Itineraries.js
+++ src/models/Itineraries.js
@@ -2,11 +2,15 @@ const { Schema, model, Types } = require("mongoose");
 
 const schemaItinery = new Schema({
     name: {type: String, required: true},
+    img: {type:String},
+    desc: {type:String},
     // userName: {type: String, required: true},
     // userPhoto: {type: String, required: true},
     price: {type: Number, required: true},
     duration: {type: String, required: true},
     comments: [{type: String, required: false}] ,
+    likes: {type:Number},
+    hastag:[{type: String}],
     _cities: {type: Types.ObjectId, ref: 'Cities'},
 })
 
