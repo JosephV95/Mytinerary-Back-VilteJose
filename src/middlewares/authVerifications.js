e329e53b536a251653ef23361591e0cf0b2bac4e
JosephV95 <viltejl@gmail.com>
2023-09-07 00:03:16 -0300

    Se creo middleware para la data del user

--- src/middlewares/authVerifications.js
+++ src/middlewares/authVerifications.js
@@ -3,13 +3,29 @@ const Joi = require('joi');
 const userSchema = Joi.object({
     name: Joi.string().required() ,
     lastname: Joi.string().required(),
-    email: Joi.email().required(),
-    password: Joi.password(),
+    email: Joi.email().required().messages({
+        'string.email': "Ingresar un email valido",
+        'string.empty': "Ingresa tu email",  //  en caso de string vacio
+        'any.required': "Se debe ingresar un email"  // caso de que se envie el campo vacio
+    }),
+    password: Joi.string().alphanum().min(6).max(12).required(),
     photo: Joi.string().uri(),
     nation: Joi.string()
 })
-const verifyAuthData = (req, res, next)=>{
 
+const verifyAuthData = (req, res, next)=>{
+    let payload = req.body;
+    
+    const userValidated = userSchema.validate(payload)
+
+    // if que se mostrara en caso de errores
+    if(userValidated.error){
+        return res.status(400).json({message: userValidated.error.details.map((error)=>{  //! el details mostrara un array con todos los errores
+            error.message
+        })}) 
+    }
+
+    next()
 }
 
 module.exports = {verifyAuthData}
--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { registerUser } = require('../controllers/auth.Controller');
+const { verifyAuthData } = require('../middlewares/authVerifications');
 
 const router = express.Router()
 
-router.post('/register', registerUser )
+router.post('/register',verifyAuthData, registerUser )
