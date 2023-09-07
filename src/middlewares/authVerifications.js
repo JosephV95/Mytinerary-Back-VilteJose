55f9c29f3cf957003bf39c7926e97ea4f8604b8e
JosephV95 <viltejl@gmail.com>
2023-09-07 11:45:11 -0300

    Se arreglo bug al mostrar el array de errores

--- src/middlewares/authVerifications.js
+++ src/middlewares/authVerifications.js
@@ -2,13 +2,16 @@ const Joi = require('joi');
 
 const userSchema = Joi.object({
     name: Joi.string().required() ,
-    lastname: Joi.string().required(),
-    email: Joi.email().required().messages({
+    lastname: Joi.string(),
+    email: Joi.string().email().required().messages({
         'string.email': "Ingresar un email valido",
         'string.empty': "Ingresa tu email",  //  en caso de string vacio
         'any.required': "Se debe ingresar un email"  // caso de que se envie el campo vacio
     }),
-    password: Joi.string().alphanum().min(6).max(12).required(),
+    password: Joi.string().alphanum().min(6).max(12).required().messages({
+        'string.min': "Ingresar como minimo 6 cifras",
+        'string.max': "Ingresar como maximo 12 cifras"
+    }),
     photo: Joi.string().uri(),
     nation: Joi.string()
 })
@@ -20,9 +23,9 @@ const verifyAuthData = (req, res, next)=>{
 
     // if que se mostrara en caso de errores
     if(userValidated.error){
-        return res.status(400).json({message: userValidated.error.details.map((error)=>{  //! el details mostrara un array con todos los errores
-            error.message
-        })}) 
+        return res.status(400).json({
+            message: userValidated.error.details.map( err => err.message )  //! el details mostrara un array con todos los errores
+        }) 
     }
 
     next()
