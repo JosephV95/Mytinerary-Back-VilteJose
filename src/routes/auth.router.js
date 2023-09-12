e8924f09e28fa159bc9bb76c03ee5b7efc972017
JosephV95 <viltejl@gmail.com>
2023-09-12 13:49:25 -0300

    Se corrigio una letra en el endpoint

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -8,7 +8,7 @@ const routerAuth = express.Router()
 routerAuth.post('/register', verifyAuthData, hashPassword, registerUser ),
 routerAuth.post('/login', verifyUserExist, verifyPassword, generateToken, loginUser),
                                 //! con 'authenticate' se indicara que trabaje con jwt y que no use session de Express
-routerAuth.post('/authenticated', passportVerificator.authenticate("jwt", {session: false}), generateToken, userAuthenticated),
+routerAuth.post('/authenticate', passportVerificator.authenticate("jwt", {session: false}), generateToken, userAuthenticated),
                         //! Verificara si esta logeado para poder Desloguarse
 routerAuth.post('/logout', passportVerificator.authenticate("jwt", {session: false}), userLogout) 
 
